c49ecd33967cfce5b7cf5df82d59fe6d571f9c69
Ricky Goldman <ricky@electriceasel.com>
2015-03-31 09:25:00 -0500

    reset gitignore cache

--- logs/error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2014-05-01 15:31:11 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority	category	message
-2014-05-01T15:31:11+00:00	INFO	joomlafailure	Empty password not allowed
