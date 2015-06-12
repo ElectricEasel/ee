4a16a67c5b0617fd09137b6512b5b6fe2574aef6
Ricky Goldman <ricky@electriceasel.com>
2015-06-12 08:53:17 -0500

    adding publisher to google+ link

--- templates/ee/index.php
+++ templates/ee/index.php
@@ -213,7 +213,7 @@ $parent_class = ($parent_class_title == '') ? '' : ' ' . $parent_class_title;
 	            <ul class="pull-left social-icons-custom">
 		            <li><a class="facebook" target="_blank" href="https://www.facebook.com/ElectricEasel"></a></li>
 		            <li><a class="twitter" target="_blank" href="https://twitter.com/electriceasel1"></a></li>
-		            <li><a class="google_plus" target="_blank" href="https://plus.google.com/113840527065273591351"></a></li>
+		            <li><a class="google_plus" target="_blank" href="https://plus.google.com/113840527065273591351" rel="publisher"></a></li>
 		            <li><a class="linkedin" target="_blank" href="https://www.linkedin.com/company/electric-easel"></a></li>
 	            </ul>
 				<jdoc:include type="modules" name="footer" style="none" />
