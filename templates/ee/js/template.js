0820c8eba99b1a18e45133a4745ee5715d04c764
Ricky Goldman <ricky@electriceasel.com>
2015-08-19 09:24:15 -0500

    making header background darker when scrolled

--- templates/ee/js/template.js
+++ templates/ee/js/template.js
@@ -250,22 +250,16 @@
             $('.menu-overlay').toggleClass('expanded');
         }
 
-        var darkenHeader = false;
+        var stickyHeader = $('.header-inner');
         $(window).scroll(function(){
             var scrollTop = $(window).scrollTop();
             if (scrollTop > 88) {
-                if (darkenHeader === true) {
-                    return;
-                } else {
-                    darkenHeader = true;
-                    $('.header-inner').css('background-color','rgba(0,0,30,0.6)');
+                if(!stickyHeader.hasClass('darker')) {
+                    stickyHeader.addClass('darker');
                 }
             } else {
-                if (darkenHeader === false) {
-                    return;
-                } else {
-                    darkenHeader = false;
-                    $('.header-inner').css('background-color','rgba(0,0,30,0.2)');
+                if(stickyHeader.hasClass('darker')) {
+                    stickyHeader.removeClass('darker');
                 }
             }
         });
