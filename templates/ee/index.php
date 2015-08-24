b4e8a55377771910039c79683dc67c140b970235
Ricky Goldman <ricky@electriceasel.com>
2015-08-24 09:08:14 -0500

    ie8 styling

--- templates/ee/index.php
+++ templates/ee/index.php
@@ -123,14 +123,10 @@ else
 	<?php
 	}
 	?>
-    <!--[if IE]>
+    <!--[if lt IE 9]>
         <link rel="stylesheet" href="/templates/<?php echo $this->template ?>/css/ie.css">
-    <![endif]-->
-    <!--[if lte IE 8]>
-        <script src="/templates/<?php echo $this->template ?>/vendor/respond.js"></script>
-    <![endif]-->
-	<!--[if lt IE 9]>
-		<script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
 <?php
